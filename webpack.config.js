fa3c4024507ab3c03fdd742e8bf81dfba5cbce8d
Maxim Sofronov <imaxsof@gmail.com>
2018-10-08 16:48:25 +0300

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,38 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
 		app: './src/js/app.js'
 	},
 	output: {
-		filename: './js/[name].js'
+		filename: 'js/[name].[hash].js'
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true
+			}),
+			new OptimizeCSSAssetsPlugin({})
+		]
 	},
 	module: {
 		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
 			{
 				test: /\.less$/,
 				use: [
@@ -20,8 +44,13 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new CleanWebpackPlugin(['dist']),
 		new MiniCssExtractPlugin({
-			filename: './css/main.[hash].css'
+			filename: 'css/main.[hash].css'
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Index',
+			template: './src/html/views/index.html'
 		})
 	]
 }
