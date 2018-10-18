12100990a9bcc92430a78de00d8b69ddbfab5087
Maxim Sofronov <imaxsof@gmail.com>
2018-10-18 11:38:52 +0300

    no message

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
+
+
+import "./../components/global";
+import "./../components/scene";
+import "./../components/card";
+
 import "./../less/global.less";
+
 
 import qwe, { notify } from "./../components/notify";
 
